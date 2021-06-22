77f3474893fbf46aeb77284e098452bb19cec4ef
lucas vital <lucasvitaldemelo@gmail.com>
2021-06-22 17:38:36 -0300

    pages auth

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCFTiFHDqY0zmzBawHbYaY_69RMqGKeDaA",
-  authDomain: "letmeask-nlw-528ce.firebaseapp.com",
-  databaseURL: "https://letmeask-nlw-528ce-default-rtdb.firebaseio.com",
-  projectId: "letmeask-nlw-528ce",
-  storageBucket: "letmeask-nlw-528ce.appspot.com",
-  messagingSenderId: "829833735018",
-  appId: "1:829833735018:web:df54be32f567ba35515313",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
